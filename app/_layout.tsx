459fbe11fd3fc50bbcdfeed3ebcaef884e34e104
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-06-30 10:55:04 -0300

    chore: implemented appkit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,12 @@
+import {
+  createAppKit,
+  defaultWagmiConfig,
+} from "@reown/appkit-wagmi-react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { arbitrum, mainnet, polygon } from "@wagmi/core/chains";
+import "@walletconnect/react-native-compat";
+import { WagmiProvider } from "wagmi";
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -5,6 +14,36 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://cloud.reown.com
+const projectId = "YOUR_PROJECT_ID";
+
+// 2. Create config
+const metadata = {
+  name: "AppKit RN",
+  description: "AppKit RN Example",
+  url: "https://reown.com/appkit",
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum] as const;
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createAppKit({
+  projectId,
+  wagmiConfig,
+  defaultChain: mainnet, // Optional
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,11 +58,15 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </QueryClientProvider>
+      </WagmiProvider>
     </ThemeProvider>
   );
 }
